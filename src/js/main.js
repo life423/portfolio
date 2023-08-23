1faa31c59f024e0e49757a07f18b93051f9548f8
Andrew Clark <drew@drewclark.io>
2023-08-22 19:59:56 -0500

    Refactor CSS styles and fix responsive layout issues
    - Updated CSS styles to improve readability and maintainability
    - Fixed responsive layout issues for better user experience on different devices

--- src/js/main.js
+++ src/js/main.js
@@ -20,6 +20,7 @@ navToggler.addEventListener("click", () => {
     navList.style.transform = "translateY(-100%)";
     barsIcon.style.display = "block";
     timesIcon.style.display = "none";
+     bodyElement.classList.add("no-scroll");
   } else {
     nav.classList.add("nav-active");
     main.style.transform = `translateY(${Math.min(
@@ -29,6 +30,7 @@ navToggler.addEventListener("click", () => {
     navList.style.transform = "translateY(0)";
     barsIcon.style.display = "none";
     timesIcon.style.display = "block";
+    bodyElement.classList.remove("no-scroll");
   }
 });
 
