a45223a8a3879851ccf2b9331ed2f111c4980ad8
Andrew Clark <drew@drewclark.io>
2023-10-17 04:29:17 -0500

    Changed the code to use single quotes instead of double quotes for string literals. Also, removed unnecessary semicolons and added missing line breaks.
    Cards are showing up but not in a line or a 2 by 2 grid

--- src/js/main.js
+++ src/js/main.js
@@ -1,154 +1,153 @@
-"use strict";
+'use strict'
 
-const navToggler = document.querySelector(".nav__toggler");
-const navList = document.querySelector(".nav__list");
-const nav = document.querySelector(".nav");
-const header = document.querySelector(".header");
-const main = document.querySelector(".main");
-const bodyElement = document.querySelector(".body");
+const navToggler = document.querySelector('.nav__toggler')
+const navList = document.querySelector('.nav__list')
+const nav = document.querySelector('.nav')
+const header = document.querySelector('.header')
+const main = document.querySelector('.main')
+const bodyElement = document.querySelector('.body')
 
-const barsIcon = document.querySelector(".nav__icon--bars");
-const timesIcon = document.querySelector(".nav__icon--times");
-console.log(timesIcon)
-const vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty("--vh", `${vh}px`);
+const barsIcon = document.querySelector('.nav__icon--bars')
+const timesIcon = document.querySelector('.nav__icon--times')
 
-window.addEventListener("resize", () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
+let vh = window.innerHeight * 0.01
+document.documentElement.style.setProperty('--vh', `${vh}px`)
 
-navToggler.addEventListener("click", () => {
-  const headerHeight = header.getBoundingClientRect().height;
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+})
 
-  if (nav.classList.contains("nav-active")) {
-    nav.classList.remove("nav-active");
-    main.style.transform = "";
-    navList.style.transform = "";
-    navList.style.height = "";
-    barsIcon.style.display = "block";
-    timesIcon.style.display = "none";
-    bodyElement.classList.remove("no-scroll");
+navToggler.addEventListener('click', () => {
+  const headerHeight = header.getBoundingClientRect().height
+
+  if (nav.classList.contains('nav-active')) {
+    nav.classList.remove('nav-active')
+    main.style.transform = ''
+    navList.style.transform = ''
+    navList.style.height = ''
+    barsIcon.style.display = 'block'
+    timesIcon.style.display = 'none'
+    bodyElement.classList.remove('no-scroll')
   } else {
-    nav.classList.add("nav-active");
-    const height = "100vh";
-    const mainTranslateDistance = height;
-    navList.style.height = height;
-    main.style.transform = `translateY(${mainTranslateDistance})`;
-    barsIcon.style.display = "none";
-    timesIcon.style.display = "block";
-    bodyElement.classList.add("no-scroll");
+    nav.classList.add('nav-active')
+    const height = '100vh'
+    const mainTranslateDistance = height
+    navList.style.height = height
+    main.style.transform = `translateY(${mainTranslateDistance})`
+    barsIcon.style.display = 'none'
+    timesIcon.style.display = 'block'
+    bodyElement.classList.add('no-scroll')
   }
-});
+})
 
-window.addEventListener("resize", () => {
-  if (nav.classList.contains("nav-active")) {
-    const navListHeight = navList.getBoundingClientRect().height;
-    const headerHeight = header.getBoundingClientRect().height;
+window.addEventListener('resize', () => {
+  if (nav.classList.contains('nav-active')) {
+    const navListHeight = navList.getBoundingClientRect().height
+    const headerHeight = header.getBoundingClientRect().height
     main.style.transform = `translateY(${Math.min(
       navListHeight,
       window.innerHeight - headerHeight
-    )}px)`;
+    )}px)`
   } else {
-    main.style.transform = "";
+    main.style.transform = ''
 
-    navList.style.transform = "";
+    navList.style.transform = ''
   }
-});
+})
 
-main.addEventListener("transitionend", function () {
-  if (!nav.classList.contains("nav-active")) {
-    this.style.transform = "";
+main.addEventListener('transitionend', function () {
+  if (!nav.classList.contains('nav-active')) {
+    this.style.transform = ''
   }
-});
+})
 
 const createFaviconFromH1 = () => {
-  const h1Text = document.querySelector("h1").textContent;
-  const canvas = document.createElement("canvas");
-  canvas.width = 16;
-  canvas.height = 16;
-  const ctx = canvas.getContext("2d");
+  const h1Text = document.querySelector('h1').textContent
+  const canvas = document.createElement('canvas')
+  canvas.width = 16
+  canvas.height = 16
+  const ctx = canvas.getContext('2d')
 
-  ctx.fillStyle = "#28b42f";
+  ctx.fillStyle = '#28b42f'
 
-  ctx.font = "10px Arial";
-  ctx.textAlign = "center";
-  ctx.textBaseline = "middle";
-  ctx.fillText(h1Text, 8, 8);
+  ctx.font = '10px Arial'
+  ctx.textAlign = 'center'
+  ctx.textBaseline = 'middle'
+  ctx.fillText(h1Text, 8, 8)
 
   const link =
-    document.querySelector('link[rel="icon"]') ||
-    document.createElement("link");
-  link.rel = "icon";
-  link.href = canvas.toDataURL("image/x-icon");
+    document.querySelector('link[rel="icon"]') || document.createElement('link')
+  link.rel = 'icon'
+  link.href = canvas.toDataURL('image/x-icon')
 
   if (!document.querySelector('link[rel="icon"]')) {
-    document.head.appendChild(link);
+    document.head.appendChild(link)
   }
-};
+}
 
 // add event listener for device orientation change
-window.addEventListener("orientationchange", () => {
-  if (nav.classList.contains("nav-active")) {
-    nav.classList.remove("nav-active");
-    main.style.transform = "";
-    barsIcon.style.display = "block";
-    timesIcon.style.display = "none";
-    bodyElement.classList.remove("no-scroll");
+window.addEventListener('orientationchange', () => {
+  if (nav.classList.contains('nav-active')) {
+    nav.classList.remove('nav-active')
+    main.style.transform = ''
+    barsIcon.style.display = 'block'
+    timesIcon.style.display = 'none'
+    bodyElement.classList.remove('no-scroll')
 
     // reset height of the navigation list
-    navList.style.height = "";
+    navList.style.height = ''
   }
-});
+})
 
-createFaviconFromH1();
-
+createFaviconFromH1()
 
 // Function to alter classes
-const checkScreenWidth = e => {
+const checkScreenWidth = (e) => {
   // Get all download button containers
-  const downloadButtonContainers = Array.from(document.querySelectorAll(".projects__tile__front .projects__download-buttons"));
+  const downloadButtonContainers = Array.from(
+    document.querySelectorAll(
+      '.projects__tile__front .projects__download-buttons'
+    )
+  )
 
-  downloadButtonContainers.forEach(container => {
+  downloadButtonContainers.forEach((container) => {
     if (e.matches) {
       // If media query matches i.e., width is less than or equal to 944px
-      container.classList.remove("hidden"); // Remove 'hidden' class
-      container.classList.add("visible"); // Add 'visible' class
+      container.classList.remove('hidden') // Remove 'hidden' class
+      container.classList.add('visible') // Add 'visible' class
     } else {
       // If media query does not match i.e., width is more than 944px
-      container.classList.remove("visible"); // Remove 'visible' class
-      container.classList.add("hidden"); // Add 'hidden' class
+      container.classList.remove('visible') // Remove 'visible' class
+      container.classList.add('hidden') // Add 'hidden' class
     }
-  });
+  })
 }
 
-// // Create a MediaQueryList object
-// const breakpoint = window.matchMedia("(max-width: 944px)");
+// Create a MediaQueryList object
+const breakpoint = window.matchMedia('(max-width: 944px)')
 
-// // Call listener function at run time
-// checkScreenWidth(breakpoint);
+// Call listener function at run time
+checkScreenWidth(breakpoint)
 
-// // Attach listener function on state changes
-// breakpoint.addEventListener('change', checkScreenWidth);
+// Attach listener function on state changes
+breakpoint.addEventListener('change', checkScreenWidth)
 
-// const navbar = document.querySelector(".nav");
-// const footer = document.querySelector(".footer");
-// const tile = document.querySelector(".projects__tile");
 
 function setTileMargins() {
-  const navbarHeight = document.querySelector('.nav').offsetHeight;
-  const footerHeight = document.querySelector('.footer').offsetHeight;
-  const viewportHeight = window.innerHeight;
+  const navbarHeight = document.querySelector('.nav').offsetHeight
+  const footerHeight = document.querySelector('.footer').offsetHeight
+  const viewportHeight = window.innerHeight
 
-  const availableSpace = viewportHeight - navbarHeight - footerHeight;
-  const margin = availableSpace / 2;  // This is the equal white space above and below the tile.
+  const availableSpace = viewportHeight - navbarHeight - footerHeight
+  const margin = availableSpace / 2 // This is the equal white space above and below the tile.
 
-  const tile = document.querySelector('.projects__tile');
-  tile.style.marginTop = `${margin}px`;
-  tile.style.marginBottom = `${margin}px`;
+  const tile = document.querySelector('.projects__tile')
+  tile.style.marginTop = `${margin}px`
+  tile.style.marginBottom = `${margin}px`
 }
 
-setTileMargins(); // Call the function to set the margins
+setTileMargins() // Call the function to set the margins
 
 function setCardDimensions() {
   // get dimensions of navbar, footer, and viewport
